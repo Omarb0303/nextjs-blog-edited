5089d7e96b18a87e4549f3125368bf2654476f0d
Omarb0303 <97351642+Omarb0303@users.noreply.github.com>
2024-02-27 22:31:00 +0100

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -1,11 +1,1 @@
-import Head from 'next/head';
 
-export default function SEO({ title, description }) {
-  return (
-    <Head>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta property="og:title" content={title} />
-    </Head>
-  );
-}
